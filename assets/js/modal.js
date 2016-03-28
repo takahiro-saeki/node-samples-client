93e95814ca5b6f3a34ca40410fb6c9a292ca758c
takahiro-saeki <hiroooon69@gmail.com>
2016-03-28 17:11:15 +0900

    一旦動くまで修正。リファクタはまた後で

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,5 +1,14 @@
 export default class modal {
   constructor() {
-    
+    this.$body = $('body');
+    this.$btn = $('.postBtn');
+    this._show();
+  }
+  _show() {
+    this.$btn.on('click', () => {
+      let tmpl = $.templates('#modalTest');
+      let html = tmpl.render();
+      this.$body.append(html);
+    })
   }
 }
